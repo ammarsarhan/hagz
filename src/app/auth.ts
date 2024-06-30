27284a0877c7541b7d8604e77d13de6981a6d4ef
ammarsarhan <ammarsarhan06@gmail.com>
2024-06-30 03:52:40 +0300

    Set up authorize function in auth.ts

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,9 +1,35 @@
 import NextAuth from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+import { hashPassword } from './lib/utils'
+
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { prisma } from '@/app/prisma'
 
-
 export const { handlers, signIn, signOut, auth } = NextAuth ({
     adapter: PrismaAdapter(prisma),
-    providers: []
+    providers: [
+        Credentials ({
+            credentials: {
+                email: {},
+                password: {}
+            },
+            authorize: async (credentials) => {
+                let user = null
+                const hashed = await hashPassword(credentials.password as string)
+
+                user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email as string,
+                        password: hashed
+                    }
+                })
+
+                if (!user) {
+                    throw new Error("User not found")
+                }
+
+                return user;
+            }
+        })
+    ]
 })
