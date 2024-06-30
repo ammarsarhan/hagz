57871238b82dc3c55e1eb195078418fa4fe0f4f0
ammarsarhan <ammarsarhan06@gmail.com>
2024-06-30 03:09:10 +0300

    Finalized Prisma & Auth configuation

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,5 +1,9 @@
 import NextAuth from 'next-auth'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from '@/app/prisma'
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+
+export const { handlers, signIn, signOut, auth } = NextAuth ({
+    adapter: PrismaAdapter(prisma),
     providers: []
 })
