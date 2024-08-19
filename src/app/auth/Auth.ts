96d3f5d2616522a21bb52828f24e5acfef38a20d
ammarsarhan <ammarsarhan06@gmail.com>
2024-08-19 09:09:06 +0300

    Initial commit

---
+++ src/app/auth/Auth.ts
@@ -0,0 +1,9 @@
+// Function to create a user with passed credentials
+export function createUserWithCredentials(email: string, password: string) {
+    
+}
+
+// Function to authenticate user with passed credentials
+export function authenticateWithCredentials(email: string, password: string) {
+    
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    colors: {
+      'primary': '#204622',
+      'secondary': '#C2FF85',
+      'tertiary': '#419146',
+      'label-primary': '#F5F5F5',
+      'label-secondary': '#060E07'
+    }
+  },
+  plugins: [],
+};
+export default config;
