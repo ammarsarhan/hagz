4f24abde7924bdd8f25a21f4bb4fc31c06e22635
ammarsarhan <ammarsarhan06@gmail.com>
2026-04-17 20:37:11 +0200

    Configured Prisma and Docker to be up and running.

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,7 +2,6 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
 const app = new Hono()
-const PORT = Number(process.env.PORT || 8080);
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
@@ -10,7 +9,8 @@ app.get('/', (c) => {
 
 serve({
   fetch: app.fetch,
-  port: PORT
+  port: 8080,
+  hostname: '0.0.0.0'
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
