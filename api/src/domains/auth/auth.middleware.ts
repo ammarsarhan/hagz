273a59501ab728f4253fa6888089acac0f7883d8
ammarsarhan <ammarsarhan06@gmail.com>
2026-04-25 14:35:01 +0300

    Changed authorize middleware to scope accessToken source by client.

--- api/src/domains/auth/auth.middleware.ts
+++ api/src/domains/auth/auth.middleware.ts
@@ -10,9 +10,19 @@ const factory = createFactory<{ Variables: AccessTokenPayload }>();
 
 export const authorize = factory.createMiddleware(
     async (c, next) => {
-        const token =
-            c.req.header("Authorization")?.replace("Bearer ", "") ||
-            getCookie(c, "accessToken");
+        // Check whether we are dealing with a mobile or web client and extract the token from the intended source only.
+        const isMobile = 
+            c.req.header("X-Client-Type") === 'mobile' &&
+            !c.req.header('origin');
+
+        let token: string | undefined;
+
+        // If mobile, extract from the Bearer header value and if web, get the cookie.
+        if (isMobile) {
+            token = c.req.header("Authorization")?.replace("Bearer ", "") 
+        } else {
+            token = getCookie(c, "accessToken");
+        };
 
         if (!token) throw new UnauthorizedError("You must be signed in to access this resource.", ERROR_CODES.USER_NOT_AUTHENTICATED);
         const { id, phone } = await jwtService.verifyAccessToken(token);
