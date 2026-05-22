bc57f4ebf5de2cbe4cc57a5e5ec8b755f5e21e4f
ammarsarhan <ammarsarhan06@gmail.com>
2026-05-22 21:14:26 +0300

    Added the BookingEvent model on the Booking relation to keep track of state transitions.

--- api/src/jobs/workers/bookings.worker.ts
+++ api/src/jobs/workers/bookings.worker.ts
@@ -1,15 +1,38 @@
 import { Worker } from "bullmq";
 import { Redis } from "ioredis";
-import type { BookingJobPayload } from "@/shared/types/bookings.js";
+import { BookingEvent, type BookingJobPayload } from "@/shared/types/bookings.js";
+import prisma from "@/shared/lib/utils/prisma.js";
+import { InternalServerError } from "@/shared/lib/utils/error.js";
+import { BookingStatus } from "@/generated/prisma/enums.js";
 
 const bookingsWorker = new Worker<BookingJobPayload>("bookings", 
     async (job) => {
-
+        try {
+            switch (job.data.event) {
+                case BookingEvent.APPROVAL:
+                    return await handleApprovalExpiry(job.data.bookingId);
+                
+            }
+        } catch (error: any) {
+            // Todo: Implement better error handling for the booking job state transition failure.
+            console.log(error.message);
+        }
     },
     { 
         connection: new Redis(process.env.REDIS_URL!, { maxRetriesPerRequest: null })
     }
 );
+
+const handleApprovalExpiry = async (bookingId: string) => {
+    const booking = await prisma.booking.findUnique({ where: { id: bookingId }, include: { slots: true }});
+
+    if (!booking)
+        throw new InternalServerError("Could not find a booking with the specified ID.")
+
+    if (!booking.isApproved) {
+        const updated = await prisma.booking.update({ where: { id: bookingId }, data: { status: BookingStatus.EXPIRED } });
+    }
+}
 
 // Handle failing and mark for manual resolving.
 bookingsWorker.on("failed", async (job, err) => {
